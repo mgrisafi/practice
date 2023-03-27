773dbcb0d96f735646e7728f00a3d928d064b6b0
mgrisafi <madisongrisafi@gmail.com>
2023-03-26 22:27:10 -0500

    update attribute screen_rect

--- AlienInvasion/ship.py
+++ AlienInvasion/ship.py
@@ -9,6 +9,7 @@ class Ship:
         Initialize the ship and set its starting position.
         """
         self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
         
         # Load the ship image and get its rect.
